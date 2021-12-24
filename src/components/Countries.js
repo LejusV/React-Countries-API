74406fc920a51c5eebe1a42bab534609f0dcdc48
LejusVDP <julvandeputte@gmail.com>
2021-12-25 00:59:29 +0100

    25/12 - sort optimization MERRY CHRISTMAS

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,34 +4,19 @@ import Card from './Card';
 
 const Countries = () => {
 	const [data, setData] = useState([]);
-	const [sortedData, setSortedData] = useState([]);
-	const [playOnce, setPlayOnce] = useState(true);
 	const [rangeValue, setRangeValue] = useState(40);
 	const [selectedRadio, setSelectedRadio] = useState("");
 	const radios = ['Africa', "America", "Asia", 'Europe', "Oceania"];
 
 	useEffect(() => {
-		if (playOnce) {
 		axios
 			.get(
 			"https://restcountries.com/v2/all?fields=name,population,region,capital,flag"
 			)
 			.then((res) => {
 				setData(res.data);
-				setPlayOnce(false);
 			});
-		}
-
-		const sortedCountry = () => {
-			const countryObj = Object.keys(data).map((i) => data[i]);
-			const sortedArray = countryObj.sort((a,b) => {
-				return b.population - a.population
-			});
-			sortedArray.length = rangeValue;
-			setSortedData(sortedArray)
-		};
-		sortedCountry();
-	}, [data, rangeValue]);
+	}, []);
 	
 	return (
 		<div className='countries'>
@@ -61,12 +46,15 @@ const Countries = () => {
 				</ul>
 			</div>
 			<div className='cancel'>
-				{selectedRadio && <h5 onClick={() => setSelectedRadio("")}>Annuler Recherche</h5>}	
+				{selectedRadio && (
+					<h5 onClick={() => setSelectedRadio("")}>Annuler Recherche</h5>
+				)}	
 			</div>
 			<ul className='countries-list'>
-					{sortedData
-						.filter((country) => country.region.includes
-						(selectedRadio))
+					{data
+						.filter((country) => country.region.includes(selectedRadio))
+						.sort((a,b) => b.population - a.population)
+						.filter((country, index) => index < rangeValue)
 						.map((country) => (
 							<Card country={country} key={country.name} />
 					))}
